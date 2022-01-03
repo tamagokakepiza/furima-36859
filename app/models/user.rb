fc9fe60349139a5599f38d77870d7638be564a97
tamagokakepiza <nyororo89@gmail.com>
2022-01-04 01:38:41 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.unique.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+      transient do
+        person {Gimei.name}
+      end
+    last_name {person.last.kanji}
+    first_name {person.first.kanji}
+    last_name_kana {person.last.katakana}
+    first_name_kana {person.first.katakana}
+    birth_day {Faker::Date.backward}
+  end
+end
