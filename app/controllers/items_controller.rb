44bb201d71ffefe11f978654e96fc33d01eb20d7
tamagokakepiza <nyororo89@gmail.com>
2022-01-23 18:56:06 +0900

    Rubocopの実行し忘れ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
