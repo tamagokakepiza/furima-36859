e600daf71aff357097bb7bc788bd65448961c987
tamagokakepiza <nyororo89@gmail.com>
2022-02-03 20:19:44 +0900

    商品購入機能のレビューからの再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :purchases
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
