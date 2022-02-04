9f284d82cca8c74115052869c3dfbe8e007fb4a3
tamagokakepiza <nyororo89@gmail.com>
2022-02-04 20:51:41 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :purchases
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid'
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
